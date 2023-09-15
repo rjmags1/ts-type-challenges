e90a9fe050bc22481fd266fd19b0bda13a903c8c
RJ Maganini <maganinirj@gmail.com>
2023-09-15 13:44:47 -0500

    9/15/2023

---
+++ 02693-medium-endswith.ts
@@ -0,0 +1,20 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+    Expect<Equal<EndsWith<"abc", "bc">, true>>,
+    Expect<Equal<EndsWith<"abc", "abc">, true>>,
+    Expect<Equal<EndsWith<"abc", "d">, false>>,
+    Expect<Equal<EndsWith<"abc", "ac">, false>>,
+    Expect<Equal<EndsWith<"abc", "">, true>>,
+    Expect<Equal<EndsWith<"abc", " ">, false>>
+];
+
+// ============= Your Code Here =============
+type EndsWith<T extends string, U extends string> = U extends ""
+    ? true
+    : T extends U
+    ? true
+    : T extends `${infer FT}${infer RT}`
+    ? EndsWith<RT, U>
+    : false;
---
+++ 02757-medium-partialbykeys.ts
@@ -0,0 +1,42 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+interface User {
+    name: string;
+    age: number;
+    address: string;
+}
+
+interface UserPartialName {
+    name?: string;
+    age: number;
+    address: string;
+}
+
+interface UserPartialNameAndAge {
+    name?: string;
+    age?: number;
+    address: string;
+}
+
+type cases = [
+    Expect<Equal<PartialByKeys<User, "name">, UserPartialName>>,
+    Expect<Equal<PartialByKeys<User, "name" | "age">, UserPartialNameAndAge>>,
+    Expect<Equal<PartialByKeys<User>, Partial<User>>>,
+    // @ts-expect-error
+    Expect<Equal<PartialByKeys<User, "name" | "unknown">, UserPartialName>>
+];
+
+// ============= Your Code Here =============
+type IntersectionToObj<T> = {
+    [K in keyof T]: T[K];
+};
+type PartialByKeys<T extends {}, K extends keyof T = any> = IntersectionToObj<
+    {
+        [P in keyof T as P extends K ? P : never]?: T[P];
+    } & {
+        [P in Exclude<keyof T, K>]: T[P];
+    }
+>;
+
+type Test = PartialByKeys<User, "name">;
